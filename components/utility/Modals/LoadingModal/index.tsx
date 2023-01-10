7a8b319cb6572c3206794b0516dca0a96bf85606
Dishant Sharma <dishu5570@gmail.com>
2023-01-10 21:17:21 +0530

    info modal

---
+++ components/utility/Modals/LoadingModal/index.tsx
@@ -0,0 +1,73 @@
+import { Fragment } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+
+type Props = {
+  isOpen: boolean,
+  setIsOpen: (argo:boolean)=>void,
+}
+
+const capitalize = (s:string) => {
+  if (typeof s !== 'string') return ''
+  return s.charAt(0).toUpperCase() + s.slice(1).toLocaleLowerCase()
+}
+
+
+const LoadingModal = ({isOpen, setIsOpen}:Props) => {
+
+  function closeModal() {
+    setIsOpen(false)
+  }
+
+  function openModal() {
+    setIsOpen(true)
+  }
+
+
+  return (
+    <Transition appear 
+      show={isOpen} 
+      as={Fragment}
+      enter="transition duration-100 ease-out"
+    >
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="h-[95vh] w-[100vw] transform overflow-hidden rounded-2xl bg-gray p-6 text-left align-middle shadow-xl transition-all">
+                  
+                <div className={`h-[90vh] w-[100vw] flex justify-center bg-gray items-center`}>
+                  <div className='bg-white' id='logo'>
+                  <img src='assets/LogoImage.png'/>
+                  </div>
+                </div>
+                  
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+  )
+}
+
+export default LoadingModal
