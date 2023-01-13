65aa442c619979cd3f754083590f64ec4c9724fb
Dishant Sharma <dishu5570@gmail.com>
2023-01-14 01:06:23 +0530

    mobile screen links and info

--- components/mobile/MobileResourceCard/index.tsx
+++ components/mobile/MobileResourceCard/index.tsx
@@ -1,55 +1,47 @@
 import { Resource } from '@/hooks/Zustand'
-import React from 'react'
+import React, { useState } from 'react'
+import {BiLinkExternal} from 'react-icons/bi'
 import { LazyLoadImage } from 'react-lazy-load-image-component'
+import { formatUrl } from '@/lib/formatUrl'
+import { InfoModal } from 'components';
+import {AiFillInfoCircle} from 'react-icons/ai'
 
 type Props = {
   resource: Resource
 }
 
-const cardData = {
-  id: 18,
-  created_at: "2022-12-15T07:07:45.44912+00:00",
-  created_by: "48a42f91-87ad-41bd-973b-357bb6adbfe4",
-  url: "colorsandfonts.com",
-  tags: [
-    "all time favourite",
-    "design improvement"
-  ],
-  image_url: "https://api.lazyweb.rocks/http://colorsandfonts.com.png",
-  title: "Colors & Fonts Colors & Fonts Colors & Fonts Colors & Fonts Colors & Fonts Colors & Fonts",
-  desc: "Color and typography for Web Developers and Digital Designers.",
-  isPublicAvailable: true,
-  likes: 1,
-  isAvailableForApproval: false,
-  category: "ui/ux",
-  created_by_list: [
-    "48a42f91-87ad-41bd-973b-357bb6adbfe4"
-  ]
-}
-
-
 
 const MobileResourceCard = ({resource}: Props) => {
+  const [open, setOpen] = useState(false)
+  const handleGoto = ():void=>{
+    window.open(formatUrl(resource.url), '_blank');
+  }
   return (
-    <div className='w-[11rem] rounded-xl overflow-hidden bg-altGray'>
+    <div className='w-[11rem] relative h-[13rem] overflow-y-hidden rounded-xl overflow-hidden bg-altGray'>
       {/* <img src={resource.image_url}/> */}
       <div className='mb-[1.5rem]'>
       <LazyLoadImage
         alt={resource.title}
         height={100}
-        className='rounded-t-[20px]'
         effect="blur"
         delayTime={1000}
         placeholderSrc="assets/placeholder-website.png"
         src={resource.image_url} // use normal <img> attributes as props
         width={200} />
+        <div onClick={handleGoto} style={{backgroundColor:'rgba(32, 33, 36, 0.5)'}} className='h-[2rem] absolute top-[5px] flex items-center justify-center left-[5px] w-[2rem] rounded-full'>
+          <BiLinkExternal className='text-white'/>
+        </div>
+        <div onClick={()=>setOpen(true)} style={{backgroundColor:'rgba(32, 33, 36, 0.5)'}} className='h-[2rem] absolute top-[5px] flex items-center justify-center right-[5px] w-[2rem] rounded-full'>
+          <AiFillInfoCircle className='text-white'/>
+        </div>
       </div>
       <div className='w-[11rem] flex justify-center my-[10px]'>
-        <div className='w-[95%]'>
+        <div className='w-[90%]'>
           <h1 className='text-white font-[600]'>{resource.title.slice(0,14)}...</h1>
           <p className='text-white text-[14px] opacity-80'>{resource.desc.slice(0,45)}...</p>
         </div>
       </div>
+      <InfoModal isOpen={open} resource={resource} setIsOpen={setOpen}/>
     </div>
   )
 }
