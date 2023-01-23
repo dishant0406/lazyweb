c7d38e5051f2e7f63b054feb28142ed85860771b
Dishant Sharma <dishu5570@gmail.com>
2023-01-23 19:29:08 +0530

    avatar

--- lazyweb/pages/api/identicon.ts
+++ lazyweb/pages/api/identicon.ts
@@ -1,17 +1,17 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import nc from "next-connect";
-import { createAvatar } from '@dicebear/avatars';
-import * as style from '@dicebear/big-ears-neutral';
+import { createAvatar } from '@dicebear/core';
+import {bigEarsNeutral} from '@dicebear/collection';
 import svg64 from "svg64";
 
 const handler = nc<NextApiRequest, NextApiResponse>();
 
 handler.post(async (req, res) => {
   const { text } = req.body;
-  const data = await createAvatar(style, {
+  const data = await createAvatar(bigEarsNeutral, {
     seed: text,
   });
-  const base64 = svg64(data);
+  const base64 = svg64(data.toString());
   res.send(base64);
 })
 
