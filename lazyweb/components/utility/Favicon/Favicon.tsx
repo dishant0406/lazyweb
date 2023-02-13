a1d6d5c4f52b40c2838c67964a5f51fd02ec730c
Dishant Sharma <dishu5570@gmail.com>
2023-02-13 21:32:11 +0530

    chore: seo optimization

--- lazyweb/components/utility/Favicon/Favicon.tsx
+++ lazyweb/components/utility/Favicon/Favicon.tsx
@@ -2,7 +2,7 @@ const Favicon = (): JSX.Element => {
   return (
       <>
           <meta name="description" content="LazyWeb" />
-          <link rel="icon" href="/assets/favicon.ico" />
+          <link rel="icon" href="assets/favicon.ico" />
           <meta name="apple-mobile-web-app-title" content="Snippit"/>
           <meta name="application-name" content="lazyweb"/>
           <meta name="theme-color" content="#202124"/>
