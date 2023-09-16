dc5da79abaddb18557ad1dceba9ea4aa76991363
Dishant Sharma <dishu5570@gmail.com>
2023-09-17 01:27:15 +0530

    bookmark, share, playground added

--- lazyweb/components/desktop/ResourceList/index.tsx
+++ lazyweb/components/desktop/ResourceList/index.tsx
@@ -1,5 +1,5 @@
 import { useAllResources, useCompleteResourceLength, useSelectedTab } from '@/hooks/Zustand';
-import {ResourceListBar, ResourceCard} from 'components'
+import { ResourceListBar, ResourceCard } from 'components'
 import { useEffect } from 'react';
 import { motion, AnimatePresence, Reorder } from "framer-motion"
 import { emojiGenerator } from 'lib/emojiGenerator';
@@ -7,54 +7,54 @@ import { trackWindowScroll } from 'react-lazy-load-image-component';
 import { ScrollPosition } from 'react-lazy-load-image-component';
 
 type Props = {
-  scrollPosition:ScrollPosition
+
 }
 
-const ResourceList = ({scrollPosition}: Props) => {
-  const fetchResources = useAllResources(state=>state.setAllResources)
-  const {setCompleteResourceLength} = useCompleteResourceLength()
-  const {allResources:resources, loading} = useAllResources()
-  const {selectedTab} = useSelectedTab()
-  useEffect(()=>{
+const ResourceList = (props: Props) => {
+  const fetchResources = useAllResources(state => state.setAllResources)
+  const { setCompleteResourceLength } = useCompleteResourceLength()
+  const { allResources: resources, loading } = useAllResources()
+  const { selectedTab } = useSelectedTab()
+  useEffect(() => {
     fetchResources('all')
     setCompleteResourceLength('all')
-  },[])
+  }, [])
 
   return (
     <div>
-      <ResourceListBar/>
-      <div className={`relative w-[100%] transition-all duration-300 ${loading ? 'animate-pulse':''} justify-center`}>
-        {resources.length>=1 && <Reorder.Group values={resources} axis={'x'} onReorder={()=>{}} className='z-[1] ml-[3rem] flex gap-[1rem] flex-wrap mt-[2rem]'>
-          {resources.map((e)=>{
+      <ResourceListBar />
+      <div className={`relative w-[100%] transition-all duration-300 ${loading ? 'animate-pulse' : ''} justify-center`}>
+        {resources.length >= 1 && <Reorder.Group values={resources} axis={'x'} onReorder={() => { }} className='z-[1] ml-[3rem] flex gap-[1rem] flex-wrap mt-[2rem]'>
+          {resources.map((e) => {
             return (
-              <Reorder.Item key={e._id} value={e} initial={{ scale:0 }}
-              animate={{ scale: 1 }}
-              exit={{ scale: 0 }}>
-                <ResourceCard scrollPosition={scrollPosition} key={e._id} resource={e} description={e.desc} title={e.title} image={e.image_url} url={e.url}/>
+              <Reorder.Item key={e._id} value={e} initial={{ scale: 0 }}
+                animate={{ scale: 1 }}
+                exit={{ scale: 0 }}>
+                <ResourceCard key={e._id} resource={e} description={e.desc} title={e.title} image={e.image_url} url={e.url} />
               </Reorder.Item>
-              )
-            })} 
+            )
+          })}
         </Reorder.Group>}
         <div className="flex ml-[2rem] w-[95%] justify-start gap-[2rem] flex-wrap my-[1rem]">
-        <AnimatePresence>
-        {resources.length<1 && !loading && (
+          <AnimatePresence>
+            {resources.length < 1 && !loading && (
               <motion.div
-              initial={{ scale:0 }}
-              animate={{ scale: 1 }}
-              exit={{ scale: 0 }} className="w-[100%] mt-[2rem] flex items-center justify-center">
+                initial={{ scale: 0 }}
+                animate={{ scale: 1 }}
+                exit={{ scale: 0 }} className="w-[100%] mt-[2rem] flex items-center justify-center">
                 <div className='px-[3rem] py-[1rem] shadow-2xl bg-altGray rounded-[20px]'>
                   <p className="text-[18px] text-white">
-                    {selectedTab==='saved' && `Not Bookmarked even a single resource! ${emojiGenerator()}` }
-                    {selectedTab==='my' && `Add your personal resource and get started! ${emojiGenerator()}` }
-                    {selectedTab==='all' && 'No Resources Found! '}
-                    {selectedTab==='publish' && `This place is empty right now! ${emojiGenerator()}` }
+                    {selectedTab === 'saved' && `Not Bookmarked even a single resource! ${emojiGenerator()}`}
+                    {selectedTab === 'my' && `Add your personal resource and get started! ${emojiGenerator()}`}
+                    {selectedTab === 'all' && 'No Resources Found! '}
+                    {selectedTab === 'publish' && `This place is empty right now! ${emojiGenerator()}`}
                   </p>
                 </div>
               </motion.div>
             )}
-        </AnimatePresence>
-        {loading && selectedTab==='my' && <div className="w-[100%] mb-[2rem] flex items-center justify-center">
-          <div className="w-16 h-16 border-b-2 rounded-full animate-spin border-lightGray"></div>
+          </AnimatePresence>
+          {loading && selectedTab === 'my' && <div className="w-[100%] mb-[2rem] flex items-center justify-center">
+            <div className="w-16 h-16 border-b-2 rounded-full animate-spin border-lightGray"></div>
           </div>
           }
         </div>
@@ -63,4 +63,4 @@ const ResourceList = ({scrollPosition}: Props) => {
   )
 }
 
-export default trackWindowScroll(ResourceList)
+export default ResourceList
