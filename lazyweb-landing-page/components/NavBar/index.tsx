32294c8aa5dcf2d3329193f1b72ea9cbaa31aaee
Dishant Sharma <dishu5570@gmail.com>
2023-09-25 18:21:08 +0530

    lazyweb-landing-page

--- lazyweb-landing-page/components/NavBar/index.tsx
+++ lazyweb-landing-page/components/NavBar/index.tsx
@@ -7,24 +7,26 @@ const NavBar = (props: Props) => {
     window.location.href = "https://app.lazyweb.rocks"
   }
 
-  const handleClick = (goto:string) => {
+  const handleClick = (goto: string) => {
     //href to a classname
     const element = document.getElementById(goto)
-    element?.scrollIntoView({behavior: "smooth"})
+    element?.scrollIntoView({ behavior: "smooth" })
   }
 
 
   return (
-   <div className="w-[100vw] flex justify-center">
-     <div className="h-[64px] w-[90vw] flex items-center justify-between">
-      <img src="assets/LogoMain.webp" className="h-[45px]" alt="Lazyweb Main Logo"/>
-      <div className="md:flex hidden gap-[2rem]">
-      <p onClick={()=>handleClick('feature')} className="cursor-pointer text-gray">Features</p>
-      <p onClick={()=>handleClick('services')} className="cursor-pointer text-gray">Services</p>
+    <div className="w-[100vw] z-[10] sticky top-0 flex justify-center">
+      <div className="h-[64px] nav-back w-full px-[4rem] flex items-center justify-center md:justify-between">
+        <div className="bg-white">
+          <img src="assets/LogoMain.webp" className="h-[40px]" alt="Lazyweb Main Logo" />
+        </div>
+        <div className="md:flex hidden gap-[2rem]">
+          <p onClick={() => handleClick('feature')} className="text-white cursor-pointer">Features</p>
+          <p onClick={() => handleClick('services')} className="text-white cursor-pointer">Services</p>
+        </div>
+        <button onClick={handleGoto} className="bg-white md:flex items-center justify-center hidden font-medium text-gray rounded-lg px-[20px] h-[40px] whitespace-nowrap">Launch App</button>
       </div>
-      <button onClick={handleGoto} className="bg-gray text-white rounded-[5px] px-[25px] h-[45px] whitespace-nowrap">Start Using</button>
     </div>
-   </div>
   )
 }
 
