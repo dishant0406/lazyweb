69a8ec91557f08db0d7c3b35aee8aab58dccd788
Dishant Sharma <dishu5570@gmail.com>
2023-09-25 18:21:08 +0530

    lazyweb-landing-page

--- lazyweb-landing-page/components/Hero/index.tsx
+++ lazyweb-landing-page/components/Hero/index.tsx
@@ -1,19 +1,72 @@
+import Image from "next/image"
+import { useParallax } from "react-scroll-parallax"
+import { BsCameraVideo } from 'react-icons/bs'
+import { PiRocketLaunchLight } from 'react-icons/pi'
+
 type Props = {}
 
 const Hero = (props: Props) => {
+  const parallax = useParallax<HTMLImageElement>({
+    scale: [0.85, 1],
+    translateY: [1, 0],
+    startScroll: 0,
+    endScroll: 210,
+  })
+
+  const handleGoto = () => {
+    //href to a url using button
+    window.location.href = "https://app.lazyweb.rocks"
+  }
+
+  const handleGotoVideo = () => {
+    //href to a url using button
+    window.open("https://www.youtube.com/watch?v=LTddiz6y1UU", "_blank")
+  }
+
   return (
     <div>
       <div className="w-[100vw] flex justify-center">
-        <img src='assets/Logo.webp' className="mt-[3rem] h-[20vw] md:h-[6vw]" alt="LazyWeb Logo"/>
+        <div data-aos="flip-left" className="bg-white overflow-hidden mt-[5rem]">
+          <img src='assets/Logo.webp' className=" h-[20vw]  md:h-[4vw]" alt="LazyWeb Logo" />
+        </div>
       </div>
-        <div className="w-[100vw] flex justify-center">
-          <div className="w-[70vw] md:w-[40vw]">
-            <h2 className="text-center font-[600] md:mt-[2rem] mt-[1rem] md:leading-[3.5vw] leading-[6vw] text-[5vw] md:text-[2.5vw]">Effortlessly find developer resources at Lazyweb</h2>
+      <div className="w-[100vw] flex justify-center">
+        <div className="w-[90vw] md:w-[50vw]">
+          <h2 data-aos="fade-right" className="text-center text-white font-[600] md:mt-[2rem] mt-[1rem] md:leading-[4.5vw] leading-[8vw] text-[7vw] md:text-[3.5vw]">
+            Effortlessly. Find developer resources at Lazyweb
+          </h2>
+          <p data-aos="fade-up" className="text-center text-white opacity-50 font-[400] md:mt-[2rem] mt-[1rem] md:leading-[1.5vw] leading-[6vw] text-[4vw] md:text-[1vw]">
+            Platform that helps developers find the best resources for their projects. We have a wide range of resources that can help you build your next project.
+          </p>
+        </div>
+      </div>
+      <div className="w-[100vw] md:mt-0 mt-[1rem] flex justify-center">
+        <div data-aos="zoom-out" className="w-[70vw] md:w-[50vw] flex md:flex-row flex-col justify-center md:gap-[2rem] gap-[1rem] md:mt-[3rem] mt-[1rem]">
+          <button onClick={handleGotoVideo} className="bg-[#0d0d0e] shadow-xl hover:scale-[1.03] transition-all duration-300 flex items-center justify-center font-medium text-white rounded-lg px-[20px] h-[50px] whitespace-nowrap">
+            Watch Video
+            <BsCameraVideo className="inline text-white h-[25px] w-[25px] ml-[10px]" />
+          </button>
+          <button onClick={handleGoto} className="bg-white shadow-xl hover:scale-[1.03] transition-all duration-300 flex items-center justify-center font-medium text-gray rounded-lg px-[20px] h-[50px] whitespace-nowrap">
+            Launch App
+            <PiRocketLaunchLight className="inline text-[#0d0d0e] h-[25px] w-[25px] ml-[10px]" />
+          </button>
+        </div>
+      </div>
+      <div className="w-[100vw] flex justify-center md:mt-[3rem] mt-[2rem]">
+        <div data-aos="flip-up" data-aos-delay='300' className="relative">
+          <img src="assets/MacbookImage.webp" className="md:w-[80vw]  w-[90vw]" alt="Lazyweb Website ScreenShot" />
+          <div style={{
+            background: 'url(assets/bac-bg.webp)',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+
+          }} className="absolute top-[5.5%] flex justify-center overflow-hidden items-end right-[10.7%] w-[78.4%] h-[84.5%]">
+            <img ref={parallax.ref} src="assets/new-frame.png" className="w-[100%] h-[100%]" alt="Lazyweb Website ScreenShot" />
           </div>
+          <div className="h-[94.5%] rounded-b-none absolute glow-div top-0 rounded-3xl left-[9.5%] w-[81%]" />
         </div>
-        <div className="w-[100vw] flex justify-center md:mt-[3rem] mt-[1rem]">
-          <img src="assets/lazyweb-ss-2.webp" className="md:w-[80vw] w-[90vw]" alt="Lazyweb Website ScreenShot" />
-        </div>
+      </div>
     </div>
   )
 }
--- lazyweb-landing-page/components/WhyLazyweb/index.tsx
+++ lazyweb-landing-page/components/WhyLazyweb/index.tsx
@@ -1,19 +1,76 @@
+import Image from 'next/image'
 import React from 'react'
+import { HiOutlineSearchCircle } from 'react-icons/hi'
+import { RiJavascriptLine } from 'react-icons/ri'
 
 type Props = {}
 
 const WhyLazyweb = (props: Props) => {
   return (
-    <div className='flex md:flex-row flex-col items-center w-[100vw] justify-center gap-[2rem] mt-[3rem] md:mt-[5rem]'>
-      <div className='md:w-[30vw] w-[90vw] justify-center flex flex-col items-center md:items-start'>
-        <h2 className='md:text-start text-center md:text-[1.5vw] font-[600] text-blue-900 '>Why Lazyweb?</h2>
-        <h2 className='md:text-start text-center md:text-[2vw] font-[600] text-gray  mt-[0.5rem]'>The ultimate resource for developers looking to save time and effort on their next project</h2>
-        <h2 className='md:text-start text-center text-gray md:text-[1.25vw]  mt-[1rem]'>We are dedicated to helping developers save time and effort by providing them with a central location for finding the resources they need. So if you are looking for a particular tool or tutorial, or just want to explore what's available, we invite you to check out our collection and see what we have to offer.</h2>
+    <>
+      <div id='services' data-aos="fade-up" className='w-full flex flex-col items-center mt-[5rem]'>
+        <h1 className='text-center md:w-full w-[95%] md:text-[3.5vw] text-[7vw] font-[600] text-white'>
+          Simplify your workflow with Lazyweb<br /> Enhance your productivity
+        </h1>
+        <p className='text-center md:w-[70vw] md:text-[1.2vw] w-[95%] opacity-50 text-[4vw] font-[400] text-white mt-[1rem]'>
+          Lazyweb is a platform that helps developers find the best resources for their projects. We have a wide range of resources that can help you build your next project.
+        </p>
       </div>
-      <div className='md:w-[50vw] w-[100vw] flex flex-col items-center'>
-        <img src='assets/call-image.webp'/>
+      <div className='flex md:flex-row flex-col items-center w-[100vw] justify-between md:px-[10vw] gap-[2rem] mt-[3rem] md:mt-[5rem]'>
+
+        <div className='md:w-[30vw] w-[90vw] justify-center flex flex-col items-center md:items-start'>
+          <h2 data-aos="fade-up" className='md:text-start text-center flex items-center justify-center gap-[1rem] md:text-[1.5vw] text-[5vw]  text-white '>
+            <HiOutlineSearchCircle className='md:text-[2.2vw] text-[7vw] rounded-lg text-gray p-[0.2rem] bg-white' />
+            Search
+          </h2>
+          <h2 data-aos="fade-up" className='md:text-start text-center md:text-[2.5vw] text-[6vw] font-[600] text-white  mt-[0.5rem]'>
+            Search for resources using AI
+          </h2>
+          <h2 data-aos="fade-up" className='md:text-start text-center opacity-50 text-white md:text-[1.25vw]  mt-[1rem]'>
+            Just type in what you are looking for and we will find the best resources for you.
+          </h2>
         </div>
-    </div>
+        <div className='relative flex flex-col md:scale-[1] scale-[0.85] md:mt-[0] mt-[-2rem] items-center overflow-hidden'>
+          <Image src='/assets/grad-1.webp' width={500} height={500} className='rounded-3xl' alt='Lazyweb Website ScreenShot' />
+          <Image data-aos="fade-left" src='/assets/search1.png' width={500} height={500} className='absolute top-[10%] shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)] left-[10%] rounded-[60px]' alt='Lazyweb Website ScreenShot' />
+          <Image data-aos="fade-right" src='/assets/search2.png' width={500} height={500} className='absolute bottom-[10%] right-[10%] shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)] rounded-[60px]' alt='Lazyweb Website ScreenShot' />
+        </div>
+      </div>
+      <div className='flex md:flex-row flex-col items-center w-[100vw] justify-between md:px-[10vw] gap-[2rem] mt-[3rem] md:mt-[5rem]'>
+        <div className='md:w-[30vw] w-[90vw] justify-center flex md:hidden flex-col items-center md:items-start'>
+          <h2 data-aos="fade-up" className='md:text-start text-center flex items-center justify-center gap-[1rem] md:text-[1.5vw] text-[5vw]  text-white '>
+            <RiJavascriptLine className='md:text-[2.2vw] text-[7vw] rounded-lg text-gray p-[0.2rem] bg-white' />
+            Collaborate & Code
+          </h2>
+          <h2 data-aos="fade-up" className='md:text-start text-center md:text-[2.5vw] text-[6vw] font-[600] text-white  mt-[0.5rem]'>
+            Real Time JavaScript Playground
+          </h2>
+          <h2 data-aos="fade-up" className='md:text-start text-center opacity-50 text-white md:text-[1.25vw]  mt-[1rem]'>
+            Real time JS playground to test your code and see the output. Create room and collaborate with your friends.
+          </h2>
+        </div>
+
+        <div className='relative flex flex-col md:scale-[1] scale-[0.85] md:mt-[0] mt-[-2rem] items-center overflow-hidden'>
+          <Image src='/assets/grad-2.jpg' width={500} height={500} className='rounded-3xl' alt='Lazyweb Website ScreenShot' />
+          <Image data-aos="fade-left" src='/assets/play1.png' width={400} height={400} className='absolute top-[5%] shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)] right-[-10%] rounded-[30px]' alt='Lazyweb Website ScreenShot' />
+          <Image data-aos="zoom-out" src='/assets/play2.png' width={400} height={400} className='absolute bottom-[5%] right-[10%] shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)] rounded-[30px]' alt='Lazyweb Website ScreenShot' />
+        </div>
+        <div className='md:w-[30vw] w-[90vw] justify-center md:flex hidden flex-col items-center md:items-start'>
+          <h2 className='md:text-start text-center flex items-center justify-center gap-[1rem] md:text-[1.5vw] text-[5vw]  text-white '>
+            <RiJavascriptLine className='md:text-[2.2vw] text-[7vw] rounded-lg text-gray p-[0.2rem] bg-white' />
+            Collaborate & Code
+          </h2>
+          <h2 className='md:text-start text-center md:text-[2.5vw] text-[6vw] font-[600] text-white  mt-[0.5rem]'>
+            Real Time JavaScript Playground
+          </h2>
+          <h2 className='md:text-start text-center opacity-50 text-white md:text-[1.25vw]  mt-[1rem]'>
+            Real time JS playground to test your code and see the output. Create room and collaborate with your friends.
+          </h2>
+        </div>
+
+      </div>
+
+    </>
   )
 }
 
