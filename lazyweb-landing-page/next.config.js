15df0d8a32b5ac7956ca26b3184069297a55add1
Dishant <dishu5570@gmail.com>
2023-10-30 20:56:01 +0530

    cloudfare

--- lazyweb-landing-page/next.config.js
+++ lazyweb-landing-page/next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  swcMinify: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true
+  },
+  //image source
+  images: {
+    domains: ['api.lazyweb.rocks', 'via.placeholder.com', 'pub-e97d1a56502c4db6aa9b54e745ce2298.r2.dev'],
+  },
 }
 
 module.exports = nextConfig
--- lazyweb/next.config.js
+++ lazyweb/next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
   },
   //image source
   images: {
-    domains: ['api.lazyweb.rocks', 'via.placeholder.com'],
+    domains: ['api.lazyweb.rocks', 'via.placeholder.com', 'pub-e97d1a56502c4db6aa9b54e745ce2298.r2.dev'],
   },
 }
 
