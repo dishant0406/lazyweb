c7bacb7bd8a9b404ff725ea0b21e4cbeaf9b5929
dishant <dishu5570@gmail.com>
2024-01-27 09:36:17 +0530

    Refactor ResourceListBar component

--- lazyweb/components/desktop/ResourceListBar/index.tsx
+++ lazyweb/components/desktop/ResourceListBar/index.tsx
@@ -1,155 +1,151 @@
 type Props = {}
-import { useSelectedTab,useUserData,useAllResources, useCompleteResourceLength,useLoginModal } from 'hooks/Zustand';
+import { useSelectedTab, useUserData, useAllResources, useCompleteResourceLength, useLoginModal } from 'hooks/Zustand';
 import { event } from 'nextjs-google-analytics';
 import { useState, useEffect } from 'react';
-import {Tabs, Tab,} from "@nextui-org/react";
-import {Kbd} from "@nextui-org/react";
+import { Tabs, Tab, } from "@nextui-org/react";
+import { Kbd } from "@nextui-org/react";
 import { useRouter } from 'next/router';
 
 const ResourceListBar = (props: Props) => {
-  const {session} = useUserData()
-  const {setSelectedTab, selectedTab} = useSelectedTab()
-  const {setAllResources,allResources} = useAllResources()
-  const {completeResourceLength,setCompleteResourceLength} = useCompleteResourceLength()
-  const {setIsLoginModalOpen} = useLoginModal()
+  const { session } = useUserData()
+  const { setSelectedTab, selectedTab } = useSelectedTab()
+  const { setAllResources, allResources } = useAllResources()
+  const { completeResourceLength, setCompleteResourceLength } = useCompleteResourceLength()
+  const { setIsLoginModalOpen } = useLoginModal()
+  const [isInitialized, setIsInitialized] = useState(false);
   const router = useRouter()
   const [tabs, setTabs] = useState([
     {
-      id:1,
-      name:'All Resources',
-      slug:'all',
-      selected:true
+      id: 1,
+      name: 'All Resources',
+      slug: 'all',
+      selected: true
     },
     {
-      id:2,
-      name:'Saved Resources',
-      slug:'saved',
-      selected:false
-    },{
-      id:3,
-      name:'My Resources',
-      slug:'my',
-      selected:false
-    },{
-      id:4,
-      name:'Publish',
-      slug:'publish',
-      selected:false
+      id: 2,
+      name: 'Saved Resources',
+      slug: 'saved',
+      selected: false
+    }, {
+      id: 3,
+      name: 'My Resources',
+      slug: 'my',
+      selected: false
+    }, {
+      id: 4,
+      name: 'Publish',
+      slug: 'publish',
+      selected: false
     }
   ])
 
-  const selectionHandler = (id:number)=>{
-    if(session){
-      const newTabs = tabs.map(e=>{
+  const selectionHandler = (id: number) => {
+    if (session) {
+      const newTabs = tabs.map(e => {
         return {
           ...e,
-          selected:false
+          selected: false
         }
       })
-      newTabs[id-1].selected = true
-      setSelectedTab(newTabs[id-1].slug)
-      setAllResources(newTabs[id-1].slug)
-      setCompleteResourceLength(newTabs[id-1].slug)
-      if(newTabs[id-1].slug === 'all'){
+      newTabs[id - 1].selected = true
+      setSelectedTab(newTabs[id - 1].slug)
+      setAllResources(newTabs[id - 1].slug)
+      setCompleteResourceLength(newTabs[id - 1].slug)
+      if (newTabs[id - 1].slug === 'all') {
         router.replace({
           pathname: '/',
         }, undefined, { shallow: true })
-      }else{
+      } else {
         router.replace({
           pathname: '/',
-          query: { 
+          query: {
             ...router.query,
-            tab: newTabs[id-1].slug
-           },
+            tab: newTabs[id - 1].slug
+          },
         }, undefined, { shallow: true })
       }
       setTabs(newTabs)
-    }else{
+    } else {
       setIsLoginModalOpen(true)
     }
   }
 
-  useEffect(()=>{
-    if(!localStorage.getItem('token') && router.query.tab){
-      router.replace({
-        pathname: '/',
-      }, undefined, { shallow: true })
-    }
 
-    if(localStorage.getItem('token') && !router.query.tab){
-      router.replace({
-        pathname: '/',
-        query: { 
-          ...router.query,
-          tab: 'all'
-         },
-      }, undefined, { shallow: true })
-    }
 
-    if(localStorage.getItem('token') && router.query.tab){
-      const tab = tabs.find(e=>e.slug === router.query.tab)
-      if(tab){
-        selectionHandler(tab.id)
+  useEffect(() => {
+    // Only proceed if router.query is ready
+    if (!isInitialized && Object.keys(router.query).length > 0) {
+      setIsInitialized(true);
+
+      if (!localStorage.getItem('token') && router.query.tab) {
+        router.replace('/', undefined, { shallow: true });
+      } else if (localStorage.getItem('token') && !router.query.tab) {
+        router.replace({
+          pathname: '/',
+          query: { ...router.query, tab: 'all' },
+        }, undefined, { shallow: true });
+      } else if (localStorage.getItem('token') && router.query.tab) {
+        const tab = tabs.find(e => e.slug === router.query.tab);
+        if (tab) {
+          selectionHandler(tab.id);
+        }
       }
-    }
-  
-  
-    if(session?.email){
-      if(router.query.tab){
-        const tab = tabs.find(e=>e.slug === router.query.tab)
-        if(tab){
-          selectionHandler(tab.id)
+
+      if (session?.email && router.query.tab) {
+        const tab = tabs.find(e => e.slug === router.query.tab);
+        if (tab) {
+          selectionHandler(tab.id);
         }
       }
     }
-  }
-  ,[session?.email])
+  }, [session?.email, router.query, isInitialized]);
 
 
-  useEffect(()=>{
+
+  useEffect(() => {
     window.scrollTo({
       top: 0,
       behavior: "smooth",
     });
-  },[selectedTab])
+  }, [selectedTab])
 
   return (
     <div className="w-[100%] sticky mt-[0.5rem] items-center top-[70px] z-[2] bg-gray lazyweb-resource-list flex justify-start">
-     <div className="flex flex-col ml-[1rem] py-[0.5rem]">
-      <Tabs 
-        aria-label="Options"
-        color='default'
-        
-        variant='bordered'
-        selectedKey={tabs.find(e=>e.selected)?.slug}
-        onSelectionChange={(e)=>{
-          const tab = tabs.find(ee=>ee.slug === e)
-          if(tab){
-            selectionHandler(tab.id)
+      <div className="flex flex-col ml-[1rem] py-[0.5rem]">
+        <Tabs
+          aria-label="Options"
+          color='default'
+
+          variant='bordered'
+          selectedKey={tabs.find(e => e.selected)?.slug}
+          onSelectionChange={(e) => {
+            const tab = tabs.find(ee => ee.slug === e)
+            if (tab) {
+              selectionHandler(tab.id)
+            }
+          }}
+        >
+          {
+            tabs.map((e, i) => {
+              if (e.slug === 'publish' && !session?.isAdmin) return null
+              return (
+                <Tab key={e.slug} title={e.name}>
+                </Tab>
+              )
+            })
           }
-        }}
-      >
-        {
-          tabs.map((e,i)=>{
-            if(e.slug === 'publish' && !session?.isAdmin) return null
-            return (
-              <Tab key={e.slug} title={e.name}> 
-            </Tab>
-            )
-          })
-        }
-      </Tabs>
-    </div>  
+        </Tabs>
+      </div>
       <div className="flex scale-75 items-center ml-[0rem]">
-                      <kbd className="px-2 py-1 text-white bg-gray-900 border border-gray-700 rounded">
-                          ctrl
-                      </kbd> 
-                      <span className="ml-2 text-trueGray-200">+</span>
-                      <kbd className="px-2 py-1 ml-2 text-white bg-gray-900 border border-gray-700 rounded">
-                          space
-                      </kbd> 
-                      <span className="ml-2 text-trueGray-200">for AI search</span>
-                  </div>
+        <kbd className="px-2 py-1 text-white bg-gray-900 border border-gray-700 rounded">
+          ctrl
+        </kbd>
+        <span className="ml-2 text-trueGray-200">+</span>
+        <kbd className="px-2 py-1 ml-2 text-white bg-gray-900 border border-gray-700 rounded">
+          space
+        </kbd>
+        <span className="ml-2 text-trueGray-200">for AI search</span>
+      </div>
     </div>
   )
 }
