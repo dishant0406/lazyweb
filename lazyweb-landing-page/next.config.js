f156621dd2a3bbc331e20c784c2bb61a71899637
dishant <dishu5570@gmail.com>
2024-04-05 16:45:18 +0530

    Add rewrites for blog routes

--- lazyweb-landing-page/next.config.js
+++ lazyweb-landing-page/next.config.js
@@ -14,6 +14,18 @@ const nextConfig = {
   images: {
     domains: ['api.lazyweb.rocks', 'via.placeholder.com', 'pub-e97d1a56502c4db6aa9b54e745ce2298.r2.dev'],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/blog",
+        destination: "https://lazyweb-blogs-gygcdy0fk-lazywebs-projects.vercel.app/blog",
+      },
+      {
+        source: "/blog/:path*",
+        destination: "https://lazyweb-blogs-gygcdy0fk-lazywebs-projects.vercel.app/blog/:path*",
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
